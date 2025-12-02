1f2b9d69d544e84da9c7f319e6f6564cae6d72f2
Andrivonjy Miranto <34777143+mi2an@users.noreply.github.com>
2025-12-02 09:02:13 +0300

    Read arbitrary data from the input files, separated by a set of strings.

--- Common/Input.cs
+++ Common/Input.cs
@@ -1,13 +1,44 @@
-﻿namespace Common;
+﻿using System.Text;
+
+namespace Common;
 
 public class Input(string FileName)
 {
     public IEnumerable<string?> Lines()
     {
-        using var reader = new StreamReader(FileName);
+        using var reader = new StreamReader(FileName, Encoding.UTF8);
         while (!reader.EndOfStream)
         {
             yield return reader.ReadLine();
+        }
+    }
+    public IEnumerable<string?> Next(ISet<string> separators)
+    {
+        using var reader = new StreamReader(FileName, Encoding.UTF8);
+        StringBuilder data = new();
+        while (!reader.EndOfStream)
+        {
+            var value = reader.Read();
+            if (value == -1)
+            {
+                yield break;
+            }
+            var c = ((char)value).ToString();
+            if (!separators.Contains(c))
+            {
+                data.Append((char)value);
+                continue;
+            }
+            if (data.Length <= 0)
+            {
+                continue;
+            }
+            yield return data.ToString();
+            data.Clear();
+        }
+        if (data.Length > 0)
+        {
+            yield return data.ToString();
         }
     }
 }
