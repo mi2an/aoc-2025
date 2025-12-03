bf12e1484321495693a8db4e8b7c38e3693c5c7c
Andrivonjy Miranto <34777143+mi2an@users.noreply.github.com>
2025-12-03 08:44:54 +0300

    Day 03: Add template

---
+++ 03/D03.cs
@@ -0,0 +1,18 @@
+﻿using Common;
+
+namespace _03;
+
+public class D03 : Solver
+{
+    public override int Day => 3;
+
+    public override string Solve1(Input input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string Solve2(Input input)
+    {
+        throw new NotImplementedException();
+    }
+}
