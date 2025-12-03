e847d3e5b0ab27fd48b8354dde6fd9cce4b4aaec
Andrivonjy Miranto <34777143+mi2an@users.noreply.github.com>
2025-12-03 22:16:15 +0300

    Day 04: Preparing the template.

---
+++ 04/D04.cs
@@ -0,0 +1,19 @@
+﻿using Common;
+
+namespace _04
+{
+    public class D04 : Solver
+    {
+        public override int Day => 4;
+
+        public override string Solve1(Input input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Solve2(Input input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
