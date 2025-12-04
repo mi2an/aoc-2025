a20846af362a5de8cb0bf6866dbbb10919c70f58
Andrivonjy Miranto <34777143+mi2an@users.noreply.github.com>
2025-12-04 21:33:10 +0300

    Uniformize namespace declaration: User file-scoped namespace.

--- 02/D02.cs
+++ 02/D02.cs
@@ -1,115 +1,114 @@
 ﻿using Common;
 
-namespace _02
+namespace _02;
+
+public class D02 : Solver
 {
-    public class D02 : Solver
+    public override int Day => 2;
+
+    public override string Solve1(Input input)
     {
-        public override int Day => 2;
-
-        public override string Solve1(Input input)
-        {
-            return Solve(input, (min, max) =>
-            {
-                ulong res = 0;
-                //That's a brute force solution... I'll get back to it later... Maybe :p
-                for (var i = min; i <= max; i++)
-                {
-                    if (IsInvalid_1(i))
-                    {
-                        res += i;
-                    }
-                }
-                return res;
-            }).ToString();
-        }
-
-        public override string Solve2(Input input)
-        {
-            return Solve(input, (min, max) =>
-            {
-                ulong res = 0;
-                //Again, that's a brute force solution, and I'll get back to it later... maybe :p
-                for (var i = min; i <= max; i++)
-                {
-                    if (IsInvalid_2(i))
-                    {
-                        res += i;
-                    }
-                }
-                return res;
-            }).ToString();
-        }
-
-        private ulong Solve(Input input, Func<ulong, ulong, ulong> solve)
+        return Solve(input, (min, max) =>
         {
             ulong res = 0;
-            foreach (var data in input.Next(Separators))
+            //That's a brute force solution... I'll get back to it later... Maybe :p
+            for (var i = min; i <= max; i++)
             {
-                if (data is null) continue;
-                var s = data.Split('-');
-                var min = ulong.Parse(s[0]);
-                var max = ulong.Parse(s[1]);
-
-                res += solve(min, max);
-            }
-            return res;
-        }
-
-        private static bool IsInvalid_1(ulong number)
-        {
-            var s = number.ToString();
-            if (s.Length % 2 != 0)
-            {
-                return false; // Odd number of digits are not a duplicated sequence.
-            }
-            var half = s.Length / 2;
-
-            for (var i = 0; i < half; ++i)
-            {
-                var j = i + half;
-                if (s[i] != s[j])
+                if (IsInvalid_1(i))
                 {
-                    return false;
+                    res += i;
                 }
             }
-            return true;
-        }
+            return res;
+        }).ToString();
+    }
 
-        private static bool IsInvalid_2(ulong number)
+    public override string Solve2(Input input)
+    {
+        return Solve(input, (min, max) =>
         {
-            var s = number.ToString();
-            if (s.Length < 2)
+            ulong res = 0;
+            //Again, that's a brute force solution, and I'll get back to it later... maybe :p
+            for (var i = min; i <= max; i++)
+            {
+                if (IsInvalid_2(i))
+                {
+                    res += i;
+                }
+            }
+            return res;
+        }).ToString();
+    }
+
+    private ulong Solve(Input input, Func<ulong, ulong, ulong> solve)
+    {
+        ulong res = 0;
+        foreach (var data in input.Next(Separators))
+        {
+            if (data is null) continue;
+            var s = data.Split('-');
+            var min = ulong.Parse(s[0]);
+            var max = ulong.Parse(s[1]);
+
+            res += solve(min, max);
+        }
+        return res;
+    }
+
+    private static bool IsInvalid_1(ulong number)
+    {
+        var s = number.ToString();
+        if (s.Length % 2 != 0)
+        {
+            return false; // Odd number of digits are not a duplicated sequence.
+        }
+        var half = s.Length / 2;
+
+        for (var i = 0; i < half; ++i)
+        {
+            var j = i + half;
+            if (s[i] != s[j])
             {
                 return false;
             }
+        }
+        return true;
+    }
 
-            var lenDivs = new List<int>(s.Length) { 1 };
-            for (int r = s.Length / 2; r > 1; --r)
-            {
-                if (s.Length % r == 0)
-                {
-                    lenDivs.Add(r);
-                }
-            }
-
-            return lenDivs.Any(n =>
-            {
-                for (var i = 0; i < n; ++i)
-                {
-                    char c = s[i];
-
-                    for (int j = i + n; j < s.Length; j += n)
-                    {
-                        if (s[j] != c)
-                        {
-                            return false;
-                        }
-                    }
-                }
-                return true;
-            });
+    private static bool IsInvalid_2(ulong number)
+    {
+        var s = number.ToString();
+        if (s.Length < 2)
+        {
+            return false;
         }
 
-        private readonly HashSet<string> Separators = [","];
+        var lenDivs = new List<int>(s.Length) { 1 };
+        for (int r = s.Length / 2; r > 1; --r)
+        {
+            if (s.Length % r == 0)
+            {
+                lenDivs.Add(r);
+            }
+        }
+
+        return lenDivs.Any(n =>
+        {
+            for (var i = 0; i < n; ++i)
+            {
+                char c = s[i];
+
+                for (int j = i + n; j < s.Length; j += n)
+                {
+                    if (s[j] != c)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        });
     }
+
+    private readonly HashSet<string> Separators = [","];
 }
--- 04/D04.cs
+++ 04/D04.cs
@@ -1,85 +1,84 @@
 ﻿using Common;
 
-namespace _04
+namespace _04;
+
+public class D04 : Solver
 {
-    public class D04 : Solver
+    public override int Day => 4;
+
+    public override string Solve1(Input input)
     {
-        public override int Day => 4;
-
-        public override string Solve1(Input input)
-        {
-            var rolls = input.Lines()
-                    .Where(l => l is not null)!
-                    .Select<string, char[]>(l => ['.', .. l.ToCharArray(), '.']).ToArray();
-            string t = new('.', rolls[0].Length);
-            return Process([
-                t.ToCharArray()
-                , ..rolls
-                , t.ToCharArray()
-            ]).ToString();
-        }
-
-        public override string Solve2(Input input)
-        {
-            var res = 0;
-            var rolls = input.Lines()
+        var rolls = input.Lines()
                 .Where(l => l is not null)!
-                .Select<string, char[]>(l => ['.', .. l.ToCharArray(), '.'])
-                .ToArray();
-            string t = new('.', rolls[0].Length);
-            rolls = [t.ToCharArray(), .. rolls, t.ToCharArray()];
+                .Select<string, char[]>(l => ['.', .. l.ToCharArray(), '.']).ToArray();
+        string t = new('.', rolls[0].Length);
+        return Process([
+            t.ToCharArray()
+            , ..rolls
+            , t.ToCharArray()
+        ]).ToString();
+    }
 
-            int cc;
-            do
-            {
-                cc = 0;
-                cc = Process(rolls);
-                res += cc;
-            } while (cc != 0);
-            return res.ToString();
-        }
+    public override string Solve2(Input input)
+    {
+        var res = 0;
+        var rolls = input.Lines()
+            .Where(l => l is not null)!
+            .Select<string, char[]>(l => ['.', .. l.ToCharArray(), '.'])
+            .ToArray();
+        string t = new('.', rolls[0].Length);
+        rolls = [t.ToCharArray(), .. rolls, t.ToCharArray()];
 
-        private static int Process(char[][] rolls)
+        int cc;
+        do
         {
-            var r = rolls.Length;
-            var c = rolls[0].Length;
+            cc = 0;
+            cc = Process(rolls);
+            res += cc;
+        } while (cc != 0);
+        return res.ToString();
+    }
 
-            var res = 0;
-            for (int i = r - 2; i > 0; --i)
+    private static int Process(char[][] rolls)
+    {
+        var r = rolls.Length;
+        var c = rolls[0].Length;
+
+        var res = 0;
+        for (int i = r - 2; i > 0; --i)
+        {
+            for (int j = c - 2; j > 0; --j)
             {
-                for (int j = c - 2; j > 0; --j)
+                if (rolls[i][j] != '@') continue;
+
+                var t = 0;
+
+                if (rolls[i - 1][j - 1] != '.') t++;
+                if (rolls[i - 1][j] != '.') t++;
+                if (rolls[i - 1][j + 1] != '.') t++;
+
+                if (rolls[i][j - 1] != '.') t++;
+                //
+                if (rolls[i][j + 1] != '.') t++;
+
+                if (rolls[i + 1][j - 1] != '.') t++;
+                if (rolls[i + 1][j] != '.') t++;
+                if (rolls[i + 1][j + 1] != '.') t++;
+
+                if (t < 4)
                 {
-                    if (rolls[i][j] != '@') continue;
-
-                    var t = 0;
-
-                    if (rolls[i - 1][j - 1] != '.') t++;
-                    if (rolls[i - 1][j] != '.') t++;
-                    if (rolls[i - 1][j + 1] != '.') t++;
-
-                    if (rolls[i][j - 1] != '.') t++;
-                    //
-                    if (rolls[i][j + 1] != '.') t++;
-
-                    if (rolls[i + 1][j - 1] != '.') t++;
-                    if (rolls[i + 1][j] != '.') t++;
-                    if (rolls[i + 1][j + 1] != '.') t++;
-
-                    if (t < 4)
-                    {
-                        rolls[i][j] = 'X';
-                        ++res;
-                    }
+                    rolls[i][j] = 'X';
+                    ++res;
                 }
             }
-            for (int i = r - 2; i > 0; --i)
-            {
-                for (int j = c - 2; j > 0; --j)
-                {
-                    if (rolls[i][j] == 'X') rolls[i][j] = '.';
-                }
-            }
-            return res;
         }
+        for (int i = r - 2; i > 0; --i)
+        {
+            for (int j = c - 2; j > 0; --j)
+            {
+                if (rolls[i][j] == 'X') rolls[i][j] = '.';
+            }
+        }
+        return res;
     }
 }
