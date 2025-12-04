c7dc908ea98802edef355b491dd0b8821e4c1d47
Andrivonjy Miranto <34777143+mi2an@users.noreply.github.com>
2025-12-04 09:46:11 +0300

    Day 04: Refactored for a more generic solution.
    I was trying to minimize memory usage as much as possible, but it seems I can't.
    So let's use the less memory-efficient one as long as it removes redundancy.

--- 04/D04.cs
+++ 04/D04.cs
@@ -8,54 +8,15 @@ namespace _04
 
         public override string Solve1(Input input)
         {
-            var res = 0;
-            var f = "";
-            var c = "";
-            foreach (var line in input.Lines())
-            {
-                if (line is null) continue;
-
-                if (c == "")
-                {
-                    f = new('.', line.Length + 2);
-                    c = '.' + line + '.';
-                    continue;
-                }
-
-                var n = '.' + line + '.';
-                res += Process(f, c, n);
-
-                f = c;
-                c = n;
-            }
-            res += Process(f, c, new('.', c.Length + 2));
-            return res.ToString();
-        }
-
-        private static int Process(string f, string c, string n)
-        {
-            var res = 0;
-            for (int i = 1; i < c.Length - 1; ++i)
-            {
-                if (c[i] != '@') continue;
-
-                var t = 0;
-
-                if (f[i - 1] == '@') t++;
-                if (f[i] == '@') t++;
-                if (f[i + 1] == '@') t++;
-
-                if (c[i - 1] == '@') t++;
-                //
-                if (c[i + 1] == '@') t++;
-
-                if (n[i - 1] == '@') t++;
-                if (n[i] == '@') t++;
-                if (n[i + 1] == '@') t++;
-
-                if (t < 4) ++res;
-            }
-            return res;
+            var rolls = input.Lines()
+                    .Where(l => l is not null)!
+                    .Select<string, char[]>(l => ['.', .. l.ToCharArray(), '.']).ToArray();
+            string t = new('.', rolls[0].Length);
+            return Process([
+                t.ToCharArray()
+                , ..rolls
+                , t.ToCharArray()
+            ]).ToString();
         }
 
         public override string Solve2(Input input)
@@ -72,13 +33,13 @@ namespace _04
             do
             {
                 cc = 0;
-                cc = Process2(rolls);
+                cc = Process(rolls);
                 res += cc;
             } while (cc != 0);
             return res.ToString();
         }
 
-        private static int Process2(char[][] rolls)
+        private static int Process(char[][] rolls)
         {
             var r = rolls.Length;
             var c = rolls[0].Length;
