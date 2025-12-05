a3bba75f29502dc01ffe962437ba24fc5d177893
Andrivonjy Miranto <34777143+mi2an@users.noreply.github.com>
2025-12-05 08:43:12 +0300

    Day 05: Solved part 2

--- 05/D05.cs
+++ 05/D05.cs
@@ -38,6 +38,44 @@ public class D05 : Solver
 
     public override string Solve2(Input input)
     {
-        throw new NotImplementedException();
+        var res = 0ul;
+        List<(ulong Min, ulong Max)> inventory = [];
+        foreach (var line in input.Lines())
+        {
+            if (line is null) continue;
+            if (string.IsNullOrWhiteSpace(line)) break;
+            var ns = line.Split('-');
+            inventory.Add((ulong.Parse(ns[0]), ulong.Parse(ns[1])));
+        }
+
+        var ic = inventory.Count;
+        bool merged;
+        do
+        {
+            merged = false;
+            for (int i = 0; i < ic; ++i)
+            {
+                for (int j = i + 1; j < ic; ++j)
+                {
+                    var (rm, rM) = inventory[i];
+                    var (bm, bM) = inventory[j];
+                    if (rm <= bM && bm <= rM)
+                    {
+                        inventory[i] = (Math.Min(rm, bm), Math.Max(rM, bM));
+                        inventory[j] = inventory[ic - 1];
+                        --ic;
+                        --j;
+                        merged = true;
+                    }
+                }
+            }
+        } while (merged);
+
+        for (int i = 0; i < ic; ++i)
+        {
+            var (rm, rM) = inventory[i];
+            res += (rM - rm + 1);
+        }
+        return res.ToString();
     }
 }
