f185349485ed534188753eeee03c70d4283d6549
Andrivonjy Miranto <34777143+mi2an@users.noreply.github.com>
2025-12-05 23:16:25 +0300

    Prepare for Day 07.
    Time will be scarce this weekend.

---
+++ 07/D07.cs
@@ -0,0 +1,18 @@
+﻿using Common;
+
+namespace _07;
+
+public class D07 : Solver
+{
+    public override int Day => 7;
+
+    public override string Solve1(Input input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string Solve2(Input input)
+    {
+        throw new NotImplementedException();
+    }
+}
