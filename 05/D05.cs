76df8e7a24e74576a28a36f2ff6a5bdcb5c9fc83
Andrivonjy Miranto <34777143+mi2an@users.noreply.github.com>
2025-12-05 08:25:30 +0300

    Day 05: Solved part 1 (brute force)

--- 05/D05.cs
+++ 05/D05.cs
@@ -8,7 +8,32 @@ public class D05 : Solver
 
     public override string Solve1(Input input)
     {
-        throw new NotImplementedException();
+        var res = 0;
+        bool fresh = true;
+        List<(ulong Min, ulong Max)> inventory = [];
+        foreach (var line in input.Lines())
+        {
+            if (line is null) continue;
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                fresh = false;
+                continue;
+            }
+            if (fresh)
+            {
+                var ns = line.Split('-');
+                inventory.Add((ulong.Parse(ns[0]), ulong.Parse(ns[1])));
+                continue;
+            }
+
+            //We use a brute force approach here (for now?), so let's just use LINQ to simplify things
+            var v = ulong.Parse(line);
+            if (inventory.Any(mm => mm.Min <= v && v <= mm.Max))
+            {
+                ++res;
+            }
+        }
+        return res.ToString();
     }
 
     public override string Solve2(Input input)
