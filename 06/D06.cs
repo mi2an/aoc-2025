46b8c27e503a6c05384e53a70d369f4786486ffc
Andrivonjy Miranto <34777143+mi2an@users.noreply.github.com>
2025-12-05 23:10:36 +0300

    Prepare for Day 06

---
+++ 06/D06.cs
@@ -0,0 +1,18 @@
+﻿using Common;
+
+namespace _06;
+
+public class D06 : Solver
+{
+    public override int Day => 6;
+
+    public override string Solve1(Input input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string Solve2(Input input)
+    {
+        throw new NotImplementedException();
+    }
+}
