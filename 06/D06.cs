b9caf1ead57dfa82e88ca5c3ef91c1c8e86ff5ee
Andrivonjy Miranto <34777143+mi2an@users.noreply.github.com>
2025-12-08 02:28:17 +0300

    Day 06: Solved part 2

--- 06/D06.cs
+++ 06/D06.cs
@@ -1,4 +1,5 @@
-﻿using Common;
+﻿using System.Text;
+using Common;
 
 namespace _06;
 
@@ -37,6 +38,50 @@ public class D06 : Solver
     public override string Solve2(Input input)
     {
         ulong res = 0;
+
+        var lines = input.Lines().Where(l => l is not null).Select(l => l!).ToList();
+        var ops = lines.Last();
+        StringBuilder n = new();
+
+        bool isAdd;
+        var t = lines[0].Length - 1;
+        while (ops[t] == ' ') --t;
+        isAdd = ops[t] == '+';
+        ulong co = isAdd ? 0ul : 1ul;
+        for (int j = lines[0].Length - 1; j >= 0; --j)
+        {
+            if (j < t)
+            {
+                t = j;
+                while (ops[t] == ' ') --t;
+                isAdd = ops[t] == '+';
+                res += co;
+                co = isAdd ? 0ul : 1ul;
+            }
+
+            n.Clear();
+            bool hasNumber = false;
+            for (int i = 0; i < lines.Count - 1; ++i)
+            {
+                var c = lines[i][j];
+                if (c != ' ') hasNumber = true;
+
+                n.Append(c);
+            }
+
+            if (!hasNumber) continue;
+
+            if (isAdd)
+            {
+                co += ulong.Parse(n.ToString());
+            }
+            else
+            {
+                co *= ulong.Parse(n.ToString());
+            }
+        }
+
+        res += co;
         return res.ToString();
     }
 }
