0962d6be09e3d0359df88e23c4ee2215ae5e6dc9
Andrivonjy Miranto <34777143+mi2an@users.noreply.github.com>
2025-12-08 02:40:39 +0300

    Day 07: Solved part 1

--- 07/D07.cs
+++ 07/D07.cs
@@ -8,7 +8,35 @@ public class D07 : Solver
 
     public override string Solve1(Input input)
     {
-        throw new NotImplementedException();
+        int res = 0;
+        HashSet<int> tachyons = [];
+        int m = -1;
+        int M = -1;
+        foreach (var line in input.Lines())
+        {
+            if (line is null) continue;
+
+            if (m == -1)
+            {
+                m = 0;
+                M = line.Length;
+                tachyons.Add(line.IndexOf('S'));
+                continue;
+            }
+            for (int i = 0; i < M; ++i)
+            {
+                if (line[i] != '^' || !tachyons.Contains(i))
+                {
+                    continue;
+                }
+
+                ++res;
+                tachyons.Remove(i);
+                tachyons.Add(Math.Max(i - 1, 0));
+                tachyons.Add(Math.Min(i + 1, M - 1));
+            }
+        }
+        return res.ToString();
     }
 
     public override string Solve2(Input input)
