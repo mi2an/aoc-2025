baec14db14f3d9f925c3dbe8fbc99e066d42a21d
Andrivonjy Miranto <34777143+mi2an@users.noreply.github.com>
2025-12-08 01:54:31 +0300

    Day 06: Solved part 1

--- 06/D06.cs
+++ 06/D06.cs
@@ -8,11 +8,35 @@ public class D06 : Solver
 
     public override string Solve1(Input input)
     {
-        throw new NotImplementedException();
+        ulong res = 0;
+
+        var lines = input.Lines().Where(l => l is not null).Select(l => l!.Split(' ', StringSplitOptions.RemoveEmptyEntries)).ToList();
+
+        var ops = lines.Last().Select(s => s[0]).ToList();
+        for (int j = lines[0].Length - 1; j >= 0; --j)
+        {
+            var isAdd = ops[j] == '+';
+            var com = isAdd ? 0ul : 1ul;
+            for (int i = lines.Count - 2; i >= 0; --i)
+            {
+                if (isAdd)
+                {
+                    com += ulong.Parse(lines[i][j]);
+                }
+                else
+                {
+                    com *= ulong.Parse(lines[i][j]);
+                }
+            }
+            res += com;
+        }
+
+        return res.ToString();
     }
 
     public override string Solve2(Input input)
     {
-        throw new NotImplementedException();
+        ulong res = 0;
+        return res.ToString();
     }
 }
