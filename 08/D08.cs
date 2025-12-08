b00ea41d044ee5465617ed2633c680c122bed9cc
Andrivonjy Miranto <34777143+mi2an@users.noreply.github.com>
2025-12-08 13:15:41 +0300

    Day 08: Solved part 1
    I might revisit this later. I want to optimize this code.

--- 08/D08.cs
+++ 08/D08.cs
@@ -8,11 +8,66 @@ public class D08 : Solver
 
     public override string Solve1(Input input)
     {
-        throw new NotImplementedException();
+        long res = 1L;
+        var junctionBoxes = input.Lines().Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => Pos.New(x!)).ToArray();
+        PriorityQueue<(int, int), double> pq = new();
+
+        for (int i = 0; i < junctionBoxes.Length; i++)
+        {
+            for (int j = i + 1; j < junctionBoxes.Length; j++)
+            {
+                pq.Enqueue((i, j), junctionBoxes[i].Distance(junctionBoxes[j]));
+            }
+        }
+
+        Dictionary<int, HashSet<int>> groups = [];
+        for (int i = 0; i < junctionBoxes.Length; i++)
+        {
+            groups[i] = [i];
+        }
+
+        var count = Test ? 10 : 1000;
+        while (count-- > 0)
+        {
+            if (pq.TryDequeue(out var groupPair, out var distance) == false)
+            {
+                break;
+            }
+            var (i, j) = groupPair;
+
+            var fg = groups[i];
+            var sg = groups[j];
+            fg.UnionWith(sg);
+
+            foreach (var k in fg)
+            {
+                groups[k] = fg;
+            }
+        }
+        pq.Clear();
+
+        res = groups.Values.Distinct().OrderByDescending(k => k.Count).Take(3).Select(x => x.Count).Aggregate(1L, (acc, x) => acc * x);
+        return res.ToString();
     }
 
     public override string Solve2(Input input)
     {
         throw new NotImplementedException();
+    }
+
+
+    private record Pos(long X, long Y, long Z)
+    {
+        public static Pos New(string s)
+        {
+            var parts = s.Split(',').Select(long.Parse).ToArray();
+            return new Pos(parts[0], parts[1], parts[2]);
+        }
+
+        public double Distance(Pos other)
+        {
+            //We don't have to take the square root for distance comparisons, it will faster this way.
+            return Math.Pow(X - other.X, 2) + Math.Pow(Y - other.Y, 2) + Math.Pow(Z - other.Z, 2);
+        }
     }
 }
