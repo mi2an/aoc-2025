fee2b7b5475a5843c9d818a0bb500384f194d8dc
Andrivonjy Miranto <34777143+mi2an@users.noreply.github.com>
2025-12-08 03:09:59 +0300

    Day 07: Solved part 2

--- 07/D07.cs
+++ 07/D07.cs
@@ -41,6 +41,45 @@ public class D07 : Solver
 
     public override string Solve2(Input input)
     {
-        throw new NotImplementedException();
+        long res = 0;
+        Dictionary<int, long> tachyons = [];
+        int m = -1;
+        int M = -1;
+        foreach (var line in input.Lines())
+        {
+            if (line is null) continue;
+
+            if (m == -1)
+            {
+                m = 0;
+                M = line.Length;
+                tachyons.Add(line.IndexOf('S'), 1);
+                continue;
+            }
+            for (int i = 0; i < M; ++i)
+            {
+                if (line[i] != '^' || !tachyons.ContainsKey(i))
+                {
+                    continue;
+                }
+
+                ++res;
+                long count = tachyons[i];
+                tachyons.Remove(i);
+                int t = Math.Max(i - 1, 0);
+                if (!tachyons.TryAdd(t, count))
+                {
+                    tachyons[t] += count;
+                }
+                t = Math.Min(i + 1, M - 1);
+
+                if (!tachyons.TryAdd(t, count))
+                {
+                    tachyons[t] += count;
+                }
+            }
+        }
+        res = tachyons.Sum(kvp => kvp.Value);
+        return res.ToString();
     }
 }
