c4aa8f6b49040e916202d717e00d0e683a57f080
Andrivonjy Miranto <34777143+mi2an@users.noreply.github.com>
2025-12-08 03:17:57 +0300

    Prepare for Day 08

---
+++ 08/D08.cs
@@ -0,0 +1,18 @@
+﻿using Common;
+
+namespace _08;
+
+public class D08 : Solver
+{
+    public override int Day => 8;
+
+    public override string Solve1(Input input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string Solve2(Input input)
+    {
+        throw new NotImplementedException();
+    }
+}
