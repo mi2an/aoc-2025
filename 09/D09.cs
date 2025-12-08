7d5cde5f81b8ace498d5fdf35b37d82b84b5d3ce
Andrivonjy Miranto <34777143+mi2an@users.noreply.github.com>
2025-12-08 13:20:20 +0300

    Prepare for Day 09

---
+++ 09/D09.cs
@@ -0,0 +1,18 @@
+﻿using Common;
+
+namespace _09;
+
+public class D09 : Solver
+{
+    public override int Day => 09;
+
+    public override string Solve1(Input input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string Solve2(Input input)
+    {
+        throw new NotImplementedException();
+    }
+}
