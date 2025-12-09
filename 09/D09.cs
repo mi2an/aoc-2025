563a7621a914452dc9ef22bceb620f9fe2180e12
Andrivonjy Miranto <34777143+mi2an@users.noreply.github.com>
2025-12-09 22:36:07 +0300

    Day 09: Optimize part 2.
    We used maths behind it.
    We "draw" a line to the right (constant y) and change state (inside or outside) each time we cross the polygon's boundary.
    We ensure that the point lies to the left (our left) of the boundary before changing state, hence the "less than" operation.
    
    The operation doesn't change whether the boundary points up or down. We always need to use the "less than" operation.

--- 09/D09.cs
+++ 09/D09.cs
@@ -1,4 +1,5 @@
-﻿using Common;
+﻿using System.ComponentModel.Design;
+using Common;
 
 namespace _09;
 
@@ -40,97 +41,93 @@ public class D09 : Solver
             {
                 var p2 = points[j];
 
-                long[][] corners = [[p1[0], p2[1]], [p2[0], p1[1]]];
-                bool[] ok = [false, false];
-                double[] angles = [0, 0];
-
-                var mX = Math.Min(p1[0], p2[0]);
-                var mY = Math.Min(p1[1], p2[1]);
-                var MX = Math.Max(p1[0], p2[0]);
-                var MY = Math.Max(p1[1], p2[1]);
-
-                for (int k = 0; k < points.Length; ++k)
-                {
-                    var pk = points[k];
-                    var pl = points[k == 0 ? points.Length - 1 : k - 1];
-
-                    if (pk[0] > mX && pk[0] < MX && pk[1] > mY && pk[1] < MY || pl[0] > mX && pl[0] < MX && pl[1] > mY && pl[1] < MY)
-                    {
-                        //a point inside the rectangle means there is a line cutting through
-                        ok[0] = false;
-                        angles[0] = 0;
-                        break;
-                    }
-
-                    //Being here means that pk and pl are outside the candidate rectangle
-
-                    var mtX = Math.Min(pk[0], pl[0]);
-                    var MtX = Math.Max(pk[0], pl[0]);
-                    var mtY = Math.Min(pk[1], pl[1]);
-                    var MtY = Math.Max(pk[1], pl[1]);
-
-                    if (mtX == MtX && mtX > mX && mtX < MX)
-                    {
-                        if (mtY <= mY && MtY > mY || mtY < MY && MtY >= MY)
-                        {
-                            //There is a vertical line crossing the rectangle
-                            ok[0] = false;
-                            angles[0] = 0;
-                            break;
-                        }
-                    }
-                    if (mtY == MtY && mtY > mY && mtY < MY)
-                    {
-                        if (mtX <= mX && MtX > mX || mtX < MX && MtX >= MX)
-                        {
-                            //There is a horizontal line crossing the rectangle
-                            ok[0] = false;
-                            angles[0] = 0;
-                            break;
-                        }
-                    }
-
-                    for (int t = 0; t < 2; ++t)
-                    {
-                        var c = corners[t];
-                        if (pk[0] == c[0] && pk[1] == c[1] || pl[0] == c[0] && pl[1] == c[1])
-                        {
-                            ok[t] = true;
-                        }
-
-                        if (!ok[t])
-                        {
-                            var a1 = Math.Atan2(c[1] - pk[1], c[0] - pk[0]);
-                            var a2 = Math.Atan2(c[1] - pl[1], c[0] - pl[0]);
-
-                            var da = a2 - a1;
-                            while (da >= Math.PI) da -= 2 * Math.PI;
-                            while (da < -Math.PI) da += 2 * Math.PI;
-
-                            angles[t] += da;
-                        }
-                    }
-
-                    if (ok.All(o => o))
-                    {
-                        break;
-                    }
-                }
-
-                ok[0] = ok[0] || Math.Abs(Math.Abs(angles[0]) - 2 * Math.PI) < 0.01;
-                ok[1] = ok[1] || Math.Abs(Math.Abs(angles[1]) - 2 * Math.PI) < 0.01;
-
-                var (x, y) = (p1[0], p1[1]);
-                var (x1, y1) = (p2[0], p2[1]);
-                if (!ok.All(o => o))
+                if (HasCrossingLine(points, p1, p2))
                 {
                     continue;
                 }
+
+                if (!PnPoly_ForHorizontalOrVerticalLines(points, p1[0], p2[1]) || !PnPoly_ForHorizontalOrVerticalLines(points, p2[0], p1[1]))
+                {
+                    continue;
+                }
+
                 var dx = Math.Abs(p1[0] - p2[0]) + 1;
                 var dy = Math.Abs(p1[1] - p2[1]) + 1;
                 res = Math.Max(res, dx * dy);
             }
         }
         return res.ToString();
+    }
+
+    private static bool HasCrossingLine(long[][] points, long[] p1, long[] p2)
+    {
+        var mX = Math.Min(p1[0], p2[0]);
+        var mY = Math.Min(p1[1], p2[1]);
+        var MX = Math.Max(p1[0], p2[0]);
+        var MY = Math.Max(p1[1], p2[1]);
+
+        for (int k = 0; k < points.Length; ++k)
+        {
+            var pk = points[k];
+            var pl = points[k == 0 ? points.Length - 1 : k - 1];
+
+            if (pk[0] > mX && pk[0] < MX && pk[1] > mY && pk[1] < MY || pl[0] > mX && pl[0] < MX && pl[1] > mY && pl[1] < MY)
+            {
+                //a point inside the rectangle means there is a line cutting through
+                return true;
+            }
+
+            //Being here means that pk and pl are outside the candidate rectangle
+
+            var mtX = Math.Min(pk[0], pl[0]);
+            var MtX = Math.Max(pk[0], pl[0]);
+            var mtY = Math.Min(pk[1], pl[1]);
+            var MtY = Math.Max(pk[1], pl[1]);
+
+            if (mtX == MtX && mtX > mX && mtX < MX)
+            {
+                if (mtY <= mY && MtY > mY || mtY < MY && MtY >= MY)
+                {
+                    //There is a vertical line crossing the rectangle
+                    return true;
+                }
+            }
+            if (mtY == MtY && mtY > mY && mtY < MY)
+            {
+                if (mtX <= mX && MtX > mX || mtX < MX && MtX >= MX)
+                {
+                    //There is a horizontal line crossing the rectangle
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    //https://www.eecs.umich.edu/courses/eecs380/HANDOUTS/PROJ2/InsidePoly.html
+    //https://wrfranklin.org/Research/Short_Notes/pnpoly.html
+    //This algorithm has been specifically modified to only address horizontal and vertical lines.
+    private static bool PnPoly_ForHorizontalOrVerticalLines(long[][] polygon, long x, long y)
+    {
+        bool res = false;
+        for (int i = 0, j = polygon.Length - 1; i < polygon.Length; j = i, ++i)
+        {
+            var xi = polygon[i][0];
+            var yi = polygon[i][1];
+            var yj = polygon[j][1];
+
+            var is_y_between_ij = (yi <= y) && (y < yj) || (yj <= y) && (y < yi);
+            var is_horizontal_line = yj == yi;
+            var is_to_the_left_of_ij = x < xi; //Verifying "xi" is enough since this operation applies only when xi == xj, indicating a vertical line.
+
+            var is_x_correctly_placed = is_horizontal_line || is_to_the_left_of_ij;
+
+            if (is_y_between_ij && is_x_correctly_placed)
+            {
+                res = !res;
+            }
+        }
+        return res;
     }
 }
