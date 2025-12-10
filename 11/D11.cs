365e8a139e916a877deca0ab4d46675e33262393
Andrivonjy Miranto <34777143+mi2an@users.noreply.github.com>
2025-12-11 01:05:13 +0300

    Prepare for Day 11

---
+++ 11/D11.cs
@@ -0,0 +1,18 @@
+﻿using Common;
+
+namespace _11;
+
+public class D11 : Solver
+{
+    public override int Day => 11;
+
+    public override string Solve1(Input input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string Solve2(Input input)
+    {
+        throw new NotImplementedException();
+    }
+}
