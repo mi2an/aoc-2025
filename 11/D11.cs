0c2712d0ca92fde10260f4932a8ab4e2a93c754a
Andrivonjy Miranto <34777143+mi2an@users.noreply.github.com>
2025-12-11 09:45:58 +0300

    [WIP] Day 11: brute-forcing part 2... well....

--- 11/D11.cs
+++ 11/D11.cs
@@ -42,6 +42,43 @@ public class D11 : Solver
 
     public override string Solve2(Input input)
     {
-        throw new NotImplementedException();
+        ulong res = 0;
+        var forwardGraph = input.Lines().Where(l => !string.IsNullOrWhiteSpace(l)).Select(l =>
+        {
+            var s = l!.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            return (Key: s[0].Replace(":", ""), Next: s[1..]);
+        }).ToDictionary(k => k.Key, k => k.Next);
+
+        res = Visit("svr", "out", forwardGraph, ["fft", "dac"], []);
+
+        return res.ToString();
+    }
+
+    private static ulong Visit(string start, string end, Dictionary<string, string[]> graph, HashSet<string> mustVisit, HashSet<string> visiting)
+    {
+        if (start == end)
+        {
+            if (mustVisit.All(visiting.Contains))
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+        if (!graph.TryGetValue(start, out var next))
+        {
+            return 0;
+        }
+
+        ulong res = 0u;
+
+        visiting.Add(start);
+        foreach (var n in next.Where(n => !visiting.Contains(n)))
+        {
+            res += Visit(n, end, graph, mustVisit, visiting);
+        }
+        visiting.Remove(start);
+
+        return res;
     }
 }
