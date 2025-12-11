8c793f2eb5564cc9b6beaddde6799fd0d8466b2f
Andrivonjy Miranto <34777143+mi2an@users.noreply.github.com>
2025-12-11 09:19:22 +0300

    Day 11: Solved part 1

--- 11/D11.cs
+++ 11/D11.cs
@@ -8,7 +8,36 @@ public class D11 : Solver
 
     public override string Solve1(Input input)
     {
-        throw new NotImplementedException();
+        ulong res = 0;
+        var forwardGraph = input.Lines().Where(l => !string.IsNullOrWhiteSpace(l)).Select(l =>
+        {
+            var s = l!.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            return (Key: s[0].Replace(":", ""), Next: s[1..]);
+        }).ToDictionary(k => k.Key, k => k.Next);
+
+        //We assume that there is no loop.
+        Queue<string> track = new();
+        track.Enqueue("you");
+
+        while (track.TryDequeue(out var c))
+        {
+            if (c == "out")
+            {
+                ++res;
+                continue;
+            }
+
+            if (!forwardGraph.TryGetValue(c, out var connections))
+            {
+                continue;
+            }
+            foreach (var next in connections)
+            {
+                track.Enqueue(next);
+            }
+        }
+
+        return res.ToString();
     }
 
     public override string Solve2(Input input)
